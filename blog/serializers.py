7ea09aae188bf68e4627061b096a2d03a3945cc4
IslombekNv <islomjon2702@gmail.com>
2021-12-30 13:40:15 +0500

    'update'

--- blog/serializers.py
+++ blog/serializers.py
@@ -10,10 +10,7 @@ class PostModelSerializer(serializers.ModelSerializer):
 
 
 class ImageSerializer(serializers.ModelSerializer):
-    images = serializers.ListField(child=serializers.FileField(max_length=100000,
-                                                               allow_empty_file=False,
-                                                               use_url=False))
-
     class Meta:
         model = Image
         fields = '__all__'
+
