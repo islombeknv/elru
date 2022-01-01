27cdb08ba2cb1bbbc1c95ad9baadd3f06a0c84e4
IslombekNv <islomjon2702@gmail.com>
2022-01-01 11:15:11 +0500

    update

--- payments/views.py
+++ payments/views.py
@@ -8,7 +8,19 @@ from orders.models import OrderModel
 
 class CheckOrder(Paycom):
     def check_order(self, amount, account, *args, **kwargs):
-        return self.ORDER_FOUND
+        try:
+            order = OrderModel.objects.get(order_id=account['order'])
+            if order.order_id != account['order']:
+                return self.ORDER_NOT_FOND
+            elif order.order_id == account['order'] and order.price == amount / 100:
+                return self.ORDER_FOUND
+            elif order.order_id == account['order'] and order.price != amount / 100:
+                return self.INVALID_AMOUNT
+            else:
+
+                return self.ORDER_NOT_FOND
+        except:
+            return self.ORDER_NOT_FOND
 
     def successfully_payment(self, account, transaction, *args, **kwargs):
         order_id = int(account)
