dc53e4f27c976f436332cb774c8c7c688a1098fb
islombeknv <islomjon2702@gmail.com>
2022-01-05 15:41:47 +0500

    'update'

--- orders/admin.py
+++ orders/admin.py
@@ -6,5 +6,5 @@ from orders.models import OrderModel
 @admin.register(OrderModel)
 class OrderModelAdmin(admin.ModelAdmin):
     search_fields = ['order_id', 'user', 'full_name']
-    list_display = ['order_id', 'full_name', 'pay', 'price', 'phone', 'status']
+    list_display = ['order_id', 'full_name', 'phone', 'price', 'pay',  'status', 'created_at']
     list_filter = ['created_at', 'pay', 'status']
