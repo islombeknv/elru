8cd76981774fe3c9d875be69916ec19c6bde5211
islombeknv <islomjon2702@gmail.com>
2022-01-27 15:13:41 +0500

    'update'

--- blocks/views.py
+++ blocks/views.py
@@ -1,8 +1,8 @@
-from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
 from blocks.models import MainBannerModal, CollectionModel, Top100BookModel, MonthBookModel, NetworkModal, \
     ApplicationsModel
